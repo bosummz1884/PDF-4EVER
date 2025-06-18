eb157bebe54bfb4877d26bc4fe8d2d380ee1e659
bosummz1884 <Brandonjulian16@gmail.com>
2025-06-18 02:54:43 -0500

    deleted temp_oroject and removed dist/ from gitignore and also removed .env from gitignore

--- temp_project/client/src/lib/pdfWorker.ts
+++
@@ -1,7 +0,0 @@
-// Configure PDF.js worker
-import * as pdfjsLib from 'pdfjs-dist';
-
-// Use local worker file provided by user
-pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.js';
-
-export { pdfjsLib };
