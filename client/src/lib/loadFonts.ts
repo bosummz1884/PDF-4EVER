82a3438b19f7c1af2b54bbe3df56110d8376582e
bosummz1884 <brandonjulian16@gmail.com>
2025-08-09 11:35:15 -0500

    fixed available font names issue

--- client/src/lib/loadFonts.ts
+++ client/src/lib/loadFonts.ts
@@ -3,110 +3,68 @@
 import { PDFDocument, PDFFont } from "pdf-lib";
 
 // This list maps font names to the paths where the font files are located.
-// For this to work, you must have these font files in your project's `public/fonts/` directory.
 const fontPaths: Record<string, string> = {
   Arial: "/fonts/arial.ttf",
-
   Helvetica: "/fonts/helvetica.ttf",
-
   "Times New Roman": "/fonts/times-new-roman.ttf",
-
   "Courier New": "/fonts/courier-new.ttf",
-
   Verdana: "/fonts/verdana.ttf",
-
   Georgia: "/fonts/georgia.ttf",
-
   "Trebuchet MS": "/fonts/trebuchet-ms.ttf",
-
   Tahoma: "/fonts/tahoma.ttf",
-
   Impact: "/fonts/impact.ttf",
-
   "Comic Sans MS": "/fonts/comic-sans-ms.ttf",
-
   Roboto: "/fonts/roboto.ttf",
-
   "Open Sans": "/fonts/open-sans.ttf",
-
   Lato: "/fonts/lato.ttf",
-
   Montserrat: "/fonts/montserrat.ttf",
-
   Oswald: "/fonts/oswald.ttf",
-
   Raleway: "/fonts/raleway.ttf",
-
   "PT Sans": "/fonts/pt-sans.ttf",
-
   "Source Sans Pro": "/fonts/source-sans-pro.ttf",
-
   Merriweather: "/fonts/merriweather.ttf",
-
   "Noto Sans": "/fonts/noto-sans.ttf",
-
   Ubuntu: "/fonts/ubuntu.ttf",
-
   Nunito: "/fonts/nunito.ttf",
-
   "Work Sans": "/fonts/work-sans.ttf",
-
   Rubik: "/fonts/rubik.ttf",
-
   Poppins: "/fonts/poppins.ttf",
-
   Inter: "/fonts/inter.ttf",
-
   "Fira Sans": "/fonts/fira-sans.ttf",
-
   Cabin: "/fonts/cabin.ttf",
-
   "Playfair Display": "/fonts/playfair-display.ttf",
-
   "Titillium Web": "/fonts/titillium-web.ttf",
-
   Inconsolata: "/fonts/inconsolata.ttf",
-
   "IBM Plex Sans": "/fonts/ibm-plex-sans.ttf",
-
   Quicksand: "/fonts/quicksand.ttf",
-
   Assistant: "/fonts/assistant.ttf",
-
   Mukta: "/fonts/mukta.ttf",
-
   Arimo: "/fonts/arimo.ttf",
-
   Karla: "/fonts/karla.ttf",
-
   "Josefin Sans": "/fonts/josefin-sans.ttf",
-
   Manrope: "/fonts/manrope.ttf",
-
   "Zilla Slab": "/fonts/zilla-slab.ttf",
-
   "Space Grotesk": "/fonts/space-grotesk.ttf",
-
   Barlow: "/fonts/barlow.ttf",
-
   Cairo: "/fonts/cairo.ttf",
-
   "DM Sans": "/fonts/dm-sans.ttf",
-
   Mulish: "/fonts/mulish.ttf",
-
   Heebo: "/fonts/heebo.ttf",
-
   "Exo 2": "/fonts/exo-2.ttf",
-
   "Be Vietnam Pro": "/fonts/be-vietnam-pro.ttf",
-
   Anton: "/fonts/anton.ttf",
 };
 
 /**
+ * **FIXED: ADDED THIS FUNCTION AND EXPORTED IT**
+ * Returns a list of all available font names.
+ */
+export function getAvailableFontNames(): string[] {
+  return Object.keys(fontPaths);
+}
+
+/**
  * Preloads a curated list of font files and embeds them into a pdf-lib document.
- * This is essential for ensuring that any text we add to the PDF can be rendered correctly.
  * @param pdfDoc The pdf-lib document instance to embed fonts into.
  * @returns A promise that resolves with a map of font names to their embedded PDFont objects.
  */
@@ -120,7 +78,6 @@ export async function loadAndEmbedFonts(
       const response = await fetch(path);
       if (response.ok) {
         const fontBytes = await response.arrayBuffer();
-        // Embed the font data into the PDF document itself
         const embeddedFont = await pdfDoc.embedFont(fontBytes);
         fontMap[fontName] = embeddedFont;
       } else {
@@ -138,3 +95,6 @@ export async function loadAndEmbedFonts(
 
   return fontMap;
 }
+
+// Renamed the function to avoid conflicts and to be more descriptive
+export const loadFonts = loadAndEmbedFonts;
